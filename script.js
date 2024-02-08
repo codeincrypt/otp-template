edf45f7b78305288a4e394e15c79384c04a8dae6
codeincrypt <kartikkr555@gmail.com>
2024-02-08 22:51:21 +0530

    otp-template

---
+++ script.js
@@ -0,0 +1,65 @@
+//Initial references
+const input = document.querySelectorAll(".input");
+const inputField = document.querySelector(".inputfield");
+const submitButton = document.getElementById("submit");
+let inputCount = 0,
+  finalInput = "";
+
+//Update input
+const updateInputConfig = (element, disabledStatus) => {
+  element.disabled = disabledStatus;
+  if (!disabledStatus) {
+    element.focus();
+  } else {
+    element.blur();
+  }
+};
+
+input.forEach((element) => {
+  element.addEventListener("keyup", (e) => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, "");
+    let { value } = e.target;
+
+    if (value.length == 1) {
+      updateInputConfig(e.target, true);
+      if (inputCount <= 3 && e.key != "Backspace") {
+        finalInput += value;
+        if (inputCount < 3) {
+          updateInputConfig(e.target.nextElementSibling, false);
+        }
+      }
+      inputCount += 1;
+    } else if (value.length == 0 && e.key == "Backspace") {
+      finalInput = finalInput.substring(0, finalInput.length - 1);
+      if (inputCount == 0) {
+        updateInputConfig(e.target, false);
+        return false;
+      }
+      updateInputConfig(e.target, true);
+      e.target.previousElementSibling.value = "";
+      updateInputConfig(e.target.previousElementSibling, false);
+      inputCount -= 1;
+    } else if (value.length > 1) {
+      e.target.value = value.split("")[0];
+    }
+    submitButton.classList.add("hide");
+  });
+});
+
+window.addEventListener("keyup", (e) => {
+  if (inputCount > 3) {
+    submitButton.classList.remove("hide");
+    submitButton.classList.add("show");
+    if (e.key == "Backspace") {
+      finalInput = finalInput.substring(0, finalInput.length - 1);
+      updateInputConfig(inputField.lastElementChild, false);
+      inputField.lastElementChild.value = "";
+      inputCount -= 1;
+      submitButton.classList.add("hide");
+    }
+  }
+});
+
+const validateOTP = () => {
+  alert("Success");
+};
